77649736715012f1a4904b1411e136f3469ed88b
tymyevhenmpw <tymyevhen@gmail.com>
2025-01-28 23:04:44 +0100

    Commit

--- app/admin/categories/page.tsx
+++ app/admin/categories/page.tsx
@@ -6,7 +6,7 @@ const Page = async () => {
   const categories = await fetchCategoriesProperties();
 
   return (
-    <section className="w-full px-10 py-10 max-md:pb-36 max-[360px]:px-4"> 
+    <section className="w-full px-10 py-10 max-[360px]:px-4"> 
       <h1 className="w-full text-heading1-bold drop-shadow-text-blue">Категорії</h1>
         <CategoryCardList categories={categories} />
     </section>
