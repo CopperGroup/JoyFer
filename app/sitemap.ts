6d9da1baefe30da5fa107edfbed256596f46feee
tymyevhenmpw <tymyevhen@gmail.com>
2025-02-12 20:16:15 +0100

    Commit

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,27 +1,37 @@
 import { Store } from "@/constants/store";
 import { fetchCatalog } from "@/lib/actions/redis/catalog.actions";
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-    const filtredProducts: any[] = await fetchCatalog();
+    let filtredProducts: any[] = [];
+    try {
+        const response = await fetch(`${Store.domain}/api/catalog`);
+        if (response.ok) {
+        filtredProducts = await response.json();
+        } else {
+        console.error("Failed to fetch catalog for sitemap.");
+        }
+    } catch (error) {
+        console.error("Error fetching catalog:", error);
+    }
 
-    const productEntries: MetadataRoute.Sitemap = filtredProducts.map(({ _id }) => ({
-        url: `${Store.domain}/catalog/${_id}`,
-    }))
+  const productEntries: MetadataRoute.Sitemap = filtredProducts.map(({ _id }) => ({
+    url: `${Store.domain}/catalog/${_id}`,
+  }));
 
-    return [
-        {
-            url: `${Store.domain}/`
-        },
-        {
-            url: `${Store.domain}/info/contacts`
-        },
-        {
-            url: `${Store.domain}/info/warranty-services`
-        },
-        {
-            url: `${Store.domain}/info/delivery-payment`
-        },
-        ...productEntries
-    ]
+  return [
+    {
+      url: `${Store.domain}/`,
+    },
+    {
+      url: `${Store.domain}/info/contacts`,
+    },
+    {
+      url: `${Store.domain}/info/warranty-services`,
+    },
+    {
+      url: `${Store.domain}/info/delivery-payment`,
+    },
+    ...productEntries,
+  ];
 }
